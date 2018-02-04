8cdbc14d925c3a39ff41aa7da54fd8d5224edd68
45b16 <abhinavkasamsetty@gmail.com>
2018-02-04 02:15:52 -0600

    Generalized API request to function call

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # fortnite-bot
 Telegram bot that gets information on Fortnite players
 
+Talk to [@fortnite_info_bot](https://t.me/fortnite_info_bot) on Telegram
+
 Powered by [Fortnite Tracker API](https://fortnitetracker.com/site-api)
+
+/info <username> for information on the player
--- fortniteBot.js
+++ fortniteBot.js
@@ -4,20 +4,40 @@ const fetch = require('node-fetch');
 
 const bot = new TeleBot(Config.telegramToken);
 
+bot.on('/start', (msg) => {
+  msg.reply.text('/info <username> for information on the player\n');
+});
+
 bot.on(/^\/info (.+)$/, (msg, props) => {
   var user = props.match[1];
-  fetch(`https://api.fortnitetracker.com/v1/profile/pc/${user}`, {
-    method: 'GET',
-    headers: {
-      'TRN-Api-Key': Config.fortniteKey
-    }
-  }).then((res) => {
-    return res.json();
-  }).then((info) => {
+  _getUserInfo(user)
+  .then((info) => {
     console.log(info);
-  }).catch((err) => {
-    console.log(err);
-  });
+    if (info.error === 'Player Not Found') {
+      return msg.reply.text('User not found.', {asReply: true});
+    } else {
+      var res = `User: ${info.epicUserHandle}\n`;
+      res += `Matches played: ${info.lifeTimeStats[7].value}\n`;
+      res += `Time played: ${info.lifeTimeStats[12].value}\n`;
+      res += `Wins: ${info.lifeTimeStats[8].value}\n`;
+      res += `Kills: ${info.lifeTimeStats[10].value}\n`;
+      res += `K/D Ratio: ${info.lifeTimeStats[11].value}\n`;
+      return msg.reply.text(res, {asReply: true});
+    }
+  }).catch(console.error);
 });
 
 bot.start();
+
+function _getUserInfo(user) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://api.fortnitetracker.com/v1/profile/pc/${user}`, {
+      method: 'GET',
+      headers: {
+        'TRN-Api-Key': Config.fortniteKey
+      }
+    }).then(res => res.json())
+    .then(info => resolve(info))
+    .catch(err => reject(err));
+  });
+}
