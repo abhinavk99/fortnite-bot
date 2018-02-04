89108c083491347a2c92956fa546c87e13e684df
45b16 <abhinavkasamsetty@gmail.com>
2018-02-04 02:41:52 -0600

    Code now handles non alphabet usernames

--- fortniteBot.js
+++ fortniteBot.js
@@ -22,6 +22,7 @@ bot.on(/^\/user (.+)$/, (msg, props) => {
       res += `Wins: ${info.lifeTimeStats[8].value}\n`;
       res += `Kills: ${info.lifeTimeStats[10].value}\n`;
       res += `K/D Ratio: ${info.lifeTimeStats[11].value}\n`;
+      res += `Kills/Min: ${info.lifeTimeStats[12].value}\n`;
       return msg.reply.text(res, {asReply: true});
     }
   }).catch(console.error);
@@ -31,7 +32,7 @@ bot.start();
 
 function _getUserInfo(user) {
   return new Promise((resolve, reject) => {
-    fetch(`https://api.fortnitetracker.com/v1/profile/pc/${user}`, {
+    fetch(`https://api.fortnitetracker.com/v1/profile/pc/${encodeURI(user)}`, {
       method: 'GET',
       headers: {
         'TRN-Api-Key': Config.fortniteKey
