b290154649842310c66b159a7c7d4c0a18da8371
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-09-15 01:39:14 -0500

    Updated list of commands

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,20 +9,22 @@ module.exports = Object.freeze({
 /pc <username> for information on the player on PC platform
 /xbox <username> for information on the player on XBOX platform
 /ps4 <username> for information on the player on PS4 platform
-/season4 or /s5 <username> for all season 4 information on the player
 /season5 or /s5 <username> for all season 5 information on the player
 /solo <username> for player's lifetime solo stats
 /duo <username> for player's lifetime duo stats
 /squad <username> for player's lifetime squad stats
-/solos4 or /solos5 <username> for player's season 4 or 5 solo stats
-/duos4 or /duos5 <username> for player's season 4 or 5duo stats
-/squads4 or /squads5 <username> for player's season 4 or 5 squad stats
+/solos5 <username> for player's season 5 solo stats
+/duos5 <username> for player's season 5 duo stats
+/squads5 <username> for player's season 5 squad stats
 /recent or /rold <username> for player's recent match information
 /compare <username1>, <username2> to compare two players
 /rating <username> for player's TRN rating stats
 /kd <username> for player's K/D ratio stats
 /winrate <username> for player's win rate stats
 /leaderboards for leaderboard data
+/challenges for current weekly challenges
+/store for current store items
+/matches for match history
 
 You can end any command (except /set, /pc, /xbox, /ps4, /compare) with pc, xbox, or ps4 to specify the platform to search for the user.`
 });
